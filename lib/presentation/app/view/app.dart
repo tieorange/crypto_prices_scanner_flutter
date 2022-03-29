2810552f0db35a36d58398c1525658d4f238d9a4
Andrii Kovalchuk <tieorange@gmail.com>
2022-03-29 15:21:15 +0300

    Added basic login UI

--- lib/presentation/app/view/app.dart
+++ lib/presentation/app/view/app.dart
@@ -1,4 +1,4 @@
-import 'package:crypto_prices/presentation/l10n/l10n.dart';
+import 'package:crypto_prices/l10n/l10n.dart';
 import 'package:crypto_prices/presentation/login/view/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
--- lib/presentation/login/view/login_page.dart
+++ lib/presentation/login/view/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:crypto_prices/presentation/l10n/l10n.dart';
+import 'package:crypto_prices/l10n/l10n.dart';
 import 'package:crypto_prices/presentation/login/cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,16 +23,13 @@ class SignInSignUpView extends StatelessWidget {
     final l10n = context.l10n;
     return Scaffold(
       appBar: AppBar(title: Text(l10n.loginAppBarTitle)),
-      body: const Center(child: SignInView()),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () => context.read<LoginCubit>().increment(),
-            child: const Icon(Icons.add),
-          )
-        ],
+      body: const Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SignInView(),
+          ),
+        ),
       ),
     );
   }
@@ -43,8 +40,43 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final login = TextFormField(
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'E-mail',
+      ),
+    );
+    final password = TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'Password',
+      ),
+    );
+
     final theme = Theme.of(context);
     final count = context.select((LoginCubit cubit) => cubit.state);
-    return Text('Crypto scanner', style: theme.textTheme.headline4);
+    final loginButton = OutlinedButton.icon(
+      onPressed: onTapLogin,
+      icon: const Icon(Icons.login, size: 18),
+      label: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Text('Login'),
+      ),
+    );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Crypto scanner', style: theme.textTheme.headline4),
+        login,
+        password,
+        const SizedBox(height: 16),
+        loginButton,
+      ],
+    );
   }
+
+  void onTapLogin() {
+      // Respond to button press
+    }
 }
